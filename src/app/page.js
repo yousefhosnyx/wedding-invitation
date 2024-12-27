d9f104ba866f777061552d493f88595f283d880b
Youssef Hossny <ussifhossny0@gmail.com>
2024-12-27 16:28:08 +0200

    create wedding

--- src/app/page.js
+++ src/app/page.js
@@ -29,50 +29,44 @@ export default function Home() {
     "https://www.google.com/maps?q=12+Nile+Corniche,+Maadi,+Cairo+Governorate,+Egypt";
 
   return (
-    <div className="animate-fadeIn min-h-screen bg-gray-900 flex flex-col items-center justify-center text-center py-12 px-4">
-      {/* Welcome Message */}
-      <p className="text-xl md:text-3xl text-yellow-400 font-medium">
-        WELCOME TO WEDDING
-      </p>
-
-      {/* Names */}
-      <h1 className="text-4xl md:text-6xl font-bold text-gray-100 mt-4">
-        Yousef & Nancy
-      </h1>
-
-      {/* Date and Time */}
-      <p className="mt-6 text-xl md:text-3xl text-gray-400">
-        Friday, 4 April 2025
-      </p>
-      <p className="text-lg md:text-2xl text-gray-400">7:00 PM</p>
-
-      {/* Location */}
-      <p className="mt-4 text-lg md:text-xl text-gray-400">
-        Nile Corniche, Maadi, Lana Hall
-      </p>
-
+    <div className="animate-fadeIn min-h-screen bg-background flex flex-col items-center justify-center text-center py-12 px-4">
       {/* Countdown Timer */}
-
-      <div className="mt-5 flex justify-center h-[50px] items-center text-2xl md:text-3xl font-bold text-yellow-400">
-        {timeLeft ? (
-          timeLeft
-        ) : (
-          <InfinitySpin
-            visible={true}
-            width="200"
-            color="#facc15"
-            ariaLabel="infinity-spin-loading"
-          />
-        )}
+      <div className="flex gap-[5px] justify-center items-center h-[50px]">
+        <p className="text-2xl md:text-3xl ">Just</p>
+        <div className=" items-center text-2xl md:text-3xl font-bold text-yellow-400 min-w-[180px] max-w-[180px] md:min-w-[222px] md:max-w-[222px] flex justify-center">
+          {timeLeft ? (
+            timeLeft
+          ) : (
+            <InfinitySpin
+              visible={true}
+              width="200"
+              color="#facc15"
+              ariaLabel="infinity-spin-loading"
+            />
+          )}
+        </div>
+        <p className="text-2xl md:text-3xl ">Left.</p>
       </div>
+
       {/* Location Button (direct link to Google Maps) */}
-      <a
-        href={locationUrl}
-        target="_blank" // Opens Google Maps in a new tab
-        className="mt-5 bg-yellow-500 hover:bg-yellow-600 text-gray-900 font-medium py-2 px-3 md:py-3 md:px-6 rounded-lg shadow-lg transition ease-in-out transform hover:scale-105"
-      >
-        See Location
-      </a>
+      <div className="flex gap-2">
+        {" "}
+        <a
+          href={locationUrl}
+          target="_blank" // Opens Google Maps in a new tab
+          className="mt-5 bg-yellow-500 hover:bg-yellow-600 text-gray-900 font-medium py-2 px-3 md:py-3 md:px-6 rounded-lg shadow-lg transition ease-in-out transform hover:scale-105"
+        >
+          Location
+        </a>
+        <a
+          href="/wedding-invitation.jpg"
+          download="Wedding_Invitation.jpg"
+          target="_blank" // Opens Google Maps in a new tab
+          className="mt-5 bg-yellow-500 hover:bg-yellow-600 text-gray-900 font-medium py-2 px-3 md:py-3 md:px-6 rounded-lg shadow-lg transition ease-in-out transform hover:scale-105"
+        >
+          Invitiation
+        </a>
+      </div>
     </div>
   );
 }
